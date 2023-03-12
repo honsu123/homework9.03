0208ba7f03870b905f0b0c68dc4878722a497b6e
kirill <nicknamekirill@gmail.com>
2023-03-12 03:12:55 +0300

    finish

---
+++ homework34/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] GetArray()
+{
+int[] array = new int[10];
+    for (int i = 0; i< array.Length; i++ )
+    {
+        array[i]=new Random().Next(100, 1000);
+    }
+    return array;
+}
+void SearchArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i <array.Length; i++ )
+    {
+        if(array[i] % 2 == 0)
+        count++;
+    }
+    Console.WriteLine("Колво чётныйх чисел:" + count + "\n");
+}
+int[] newArray = GetArray();
+SearchArray(newArray);
+Console.WriteLine(String.Join(" ", newArray));
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = a%2;
+// Console.WriteLine(b);
---
+++ homework36/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] GetArray()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 20);
+    }
+    return array;
+}
+void SearchArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0);
+        else
+            count = count + array[i];
+    }
+    Console.WriteLine("Сумма элементов: " + count);
+}
+int[] newArray = GetArray();
+SearchArray(newArray);
+Console.WriteLine(String.Join(" ", newArray));
